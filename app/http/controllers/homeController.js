bd84de7692fde0a529be9ba6fc906dc8830ca1f5
Abhii27 <abhishekpanchal@1998>
2021-09-19 17:05:21 +0530

    Initial commit

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,27 @@
+const Menu = require("../../models/menu");
+
+function homeController() {
+  return {
+      async index(req, res) {
+          const pizzas = await Menu.find()
+          //console.log(pizzas);
+          return res.render('home', { pizzas: pizzas })
+      }
+  }
+}
+
+module.exports = homeController
+
+
+/* function homeController() {
+  return {
+    index(req, res) {
+      Menu.find().then(function (pizzas) {
+        console.log(pizzas);
+        return res.render("home", { pizzas: pizzas });
+      });
+    },
+  };
+}
+
+module.exports = homeController; */
